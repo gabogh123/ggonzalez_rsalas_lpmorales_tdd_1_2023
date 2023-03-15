63c9f429f8b38d538434021510ad007613729156
Rubén Salas <rubsalas@hotmail.com>
2023-03-15 04:40:17 -0600

    Implementación Display 7 Segmentos con Decodificador
    Se crea un modulo para pasar de un numero binario a la señal necesaria para el display de 7 segmentos. Además, se crea un modulo donde se utilizan 5 modulos de conversión para mostrar la lógica completa del decodificador BCD en los displays.

--- Laboratorio 2/Ejercicio 1/BCDDecoder.sv
+++ Laboratorio 2/Ejercicio 1/BCDDecoder.sv
@@ -1,50 +1,44 @@
-module BCDDecoder(input [3:0] w,
-						output  [0:4] y);
-						
-		always @ (w)
+module BCDDecoder(a, b, c, d, y);
+
+	input a, b, c, d;
+
+	output reg [4:0]y;
+
+	always @(a,b,c,d)
+		
 		begin
 		
-			y = 5'b00000;
-			
-			if(w == 4'b0001)
-				y = 5'b00001;
-			else if(w == 4'b0010)
-				y = 5'b00010;
-			else if(w == 4'b0011)
-				y = 5'b00011;
-			else if(w == 4'b0100)
-				y = 5'b00100;
-			else if(w == 4'b0101)
-				y = 5'b00101;
-			else if(w == 4'b0110)
-				y = 5'b00110;
-			else if(w == 4'b0111)
-				y = 5'b00111;
-			else if(w == 4'b1000)
-				y = 5'b01000;
-			else if(w == 4'b1001)
-				y = 5'b01001;
-			else if(w == 4'b1010)
-				y = 5'b10000;
-			else if(w == 4'b1011)
-				y = 5'b10001;
-			else if(w == 4'b1100)
-				y = 5'b10010;
-			else if(w == 4'b1101)
-				y = 5'b10011;
-			else if(w == 4'b1110)
-				y = 5'b10100;
-			else if(w == 4'b1111)
-				y = 5'b10101;
-			else 
-				y;
+		if(a==1'b0 & b==1'b0 & c==1'b0 & d==1'b0) 
+			y=5'b00000;
+      else if(a==1'b0 & b==1'b0 & c==1'b0 & d==1'b1) 
+			y=5'b00001;
+      else if(a==1'b0 & b==1'b0 & c==1'b1 & d==1'b0) 
+			y=5'b00010;
+      else if(a==1'b0 & b==1'b0 & c==1'b1 & d==1'b1) 
+			y=5'b00011;
+		else if(a==1'b0 & b==1'b1 & c==1'b0 & d==1'b0) 
+			y=5'b00100;
+      else if(a==1'b0 & b==1'b1 & c==1'b0 & d==1'b1) 
+			y=5'b00101;
+      else if(a==1'b0 & b==1'b1 & c==1'b1 & d==1'b0) 
+			y=5'b00110;
+		else if(a==1'b0 & b==1'b1 & c==1'b1 & d==1'b1) 
+			y=5'b00111;
+      else if(a==1'b1 & b==1'b0 & c==1'b0 & d==1'b0) 
+			y=5'b01000;
+      else if(a==1'b1 & b==1'b0 & c==1'b0 & d==1'b1) 
+			y=5'b01001;
+		else if(a==1'b1 & b==1'b0 & c==1'b1 & d==1'b0) 
+			y=5'b10000;
+      else if(a==1'b1 & b==1'b0 & c==1'b1 & d==1'b1) 
+			y=5'b10001;
+      else if(a==1'b1 & b==1'b1 & c==1'b0 & d==1'b0) 
+			y=5'b10010;
+		else if(a==1'b1 & b==1'b1 & c==1'b0 & d==1'b1) 
+			y=5'b10011;
+      else if(a==1'b1 & b==1'b1 & c==1'b1 & d==1'b0) 
+			y=5'b10100;
+      else if(a==1'b1 & b==1'b1 & c==1'b1 & d==1'b1) 
+			y=5'b10101;
 		end
 endmodule
-
-			
-			
-				
-				
-				
-				
-
