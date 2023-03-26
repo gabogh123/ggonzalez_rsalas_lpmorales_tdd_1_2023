f2542c60d130c52f3093b7f89b7c53e91fd78b25
gabogh123 <gabogh99@gmail.com>
2023-03-26 04:04:56 -0600

    changes

--- Laboratorio 3/Ejercicio 1/suma.sv
+++ Laboratorio 3/Ejercicio 1/suma.sv
@@ -1,19 +1,20 @@
 module suma
+
 #(parameter N = 4)
 (
-    input [N-1:0] input1, input2,
-    output [N-1:0] answer
+    input [N-1:0] a, b,
+    output [N-1:0] result
 );
-    wire [N-1:0] carry;// intermediate carry outputs
-    wire carry_out;// for the end carry output
-    genvar i;// general variable
+    wire [N-1:0] carry;
+    wire carry_out;
+    genvar i;
  
     generate
       for (i = 0; i < N; i = i + 1) begin: generate_suma
-        if (i == 0) // LSB bit adder
-          half_adder b(input1[0], input2[0], answer[0], carry[0]);
-        else // rest all adders
-          full_adder b(input1[i], input2[i], carry[i-1], answer[i], carry[i]);
+        if (i == 0) 
+          half_adder b(a[0], b[0], result[0], carry[0]);
+        else 
+          full_adder b(a[i], b[i], carry[i-1], result[i], carry[i]);
       end  
  
         assign carry_out = carry[N-1];
