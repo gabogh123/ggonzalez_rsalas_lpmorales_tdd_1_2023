8c6483a7860d5259afd9ab8515273689cd3d70db
gabogh123 <gabogh99@gmail.com>
2023-03-26 02:12:39 -0600

    Adder param

--- Laboratorio 3/Ejercicio 1/suma.sv
+++ Laboratorio 3/Ejercicio 1/suma.sv
@@ -1,13 +1,21 @@
-module suma_param 
-			# (parameter BITS = 4)
-			  (A, B, S);
-			  
-	input 	[BITS-1:0] 	A;
-	input 	[BITS-1:0] 	B;
-	
-	output   [BITS-1:0] 	S;
-	
-	
-	assign S = A + B; // Obvio hay que corregir, es por el ejemplo
-	
+module suma
+#(parameter N = 4)
+(
+    input [N-1:0] input1, input2,
+    output [N-1:0] answer
+);
+    wire [N-1:0] carry;// intermediate carry outputs
+    wire carry_out;// for the end carry output
+    genvar i;// general variable
+ 
+    generate
+      for (i = 0; i < N; i = i + 1) begin: generate_suma
+        if (i == 0) // LSB bit adder
+          half_adder b(input1[0], input2[0], answer[0], carry[0]);
+        else // rest all adders
+          full_adder b(input1[i], input2[i], carry[i-1], answer[i], carry[i]);
+      end  
+ 
+        assign carry_out = carry[N-1];
+    endgenerate
 endmodule
