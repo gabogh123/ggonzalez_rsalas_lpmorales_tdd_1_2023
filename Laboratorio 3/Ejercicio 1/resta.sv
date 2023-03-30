655dd0664adee401acd5355ddc59f006cd138cb4
gabogh123 <gabogh99@gmail.com>
2023-03-29 18:05:46 -0600

    Update resta.sv

--- Laboratorio 3/Ejercicio 1/resta.sv
+++ Laboratorio 3/Ejercicio 1/resta.sv
@@ -1,36 +1,35 @@
-module resta
-	# (parameter M = 4)
-	  (A, B, R, C, N, V, Z);
-  
-	input  logic [M-1:0] A;
-	input  logic [M-1:0] B;
-	
-	output logic [M-1:0] R;
-	output				   C = 0; // ?
-	output					N;
-	output					V = 0;
-	output					Z;
-	 
+module resta #(parameter M = 4) (
+    input logic [M-1:0] A,
+    input logic [M-1:0] B,
+    output logic [M-1:0] R,
+    output 					 C,
+	 output					 N,
+	 output					 V,
+	 output					 Z
+);
 
-	wire [M-1:0] A_comp;
-	wire [M-1:0] B_comp;
-	wire [M:0]    carry;
+logic [M-1:0] not_B;
+logic [M:0] temp;
 
-	assign A_comp = ~A;
-	assign B_comp = ~B;
 
-	assign carry[0] = 1'b1;
+// invert b bits
+assign not_B = ~B;
 
-	genvar i;
-	generate
-		 for (i = 0; i < M; i++) begin : subtractor
-			  assign carry[i+1] = ((A_comp[i] & carry[i]) | (B[i] & carry[i]) | (A_comp[i] & B[i]));
-			  assign R[i] = (A_comp[i] ^ B[i] ^ carry[i]);
-		 end
-		 
-		 // assign C = carry[M-1];
-		 
-	endgenerate
+// initialize borrow to 1
+assign temp[0] = 1'b1;
+
+
+// subtract each bit and propagate borrow
+generate
+    genvar i;
+    for (i = 0; i < M; i++) begin : subtract_bit
+        assign temp[i+1] = (A[i] & not_B[i]) | (~A[i] & not_B[i] & temp[i]);
+        assign R[i] = A[i] ^ B[i] ^ temp[i];
+    end
+endgenerate
+
+// borrow is the last carry out bit
+assign C = temp[M];
 
 endmodule	 
 	 
