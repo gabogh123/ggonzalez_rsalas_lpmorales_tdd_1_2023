0e746eacf073ea93c6286f20cf4e7941f86d4b2b
LuisPeMoraRod <luismorarod98@estudiantec.cr>
2023-04-17 19:57:31 -0600

    feat(vga): add vga controller with dummy display

---
+++ game_2048/output_files/game_2048.cdf
@@ -0,0 +1,15 @@
+/* Quartus Prime Version 20.1.0 Build 711 06/05/2020 SJ Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Ign)
+		Device PartName(SOCVHPS) MfrSpec(OpMask(0));
+	P ActionCode(Cfg)
+		Device PartName(5CSEMA5F31) Path("C:/Users/luism/source/repos/TDD_projects/game_2048/output_files/") File("game_2048.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
---
+++ game_2048/vga.sv
@@ -0,0 +1,25 @@
+module vga(
+		input logic clk, rst,
+		output logic vgaclk, // 25.175 MHz VGA clock
+		output logic hsync, vsync,
+		output logic sync_b, blank_b, // To monitor & DAC
+		output logic [7:0] r, g, b // To video DAC
+	); 
+	
+	logic [9:0] x, y;
+	
+	// Use a PLL to create the 25.175 MHz VGA pixel clock
+	// 25.175 MHz clk period = 39.772 ns
+	// Screen is 800 clocks wide by 525 tall, but only 640 x 480 used
+	// HSync = 1/(39.772 ns *800) = 31.470 kHz
+	// Vsync = 31.474 kHz / 525 = 59.94 Hz (~60 Hz refresh rate)
+	// this clock dividers generates a 25MHz clock (difference with 25.175MHz will considered as negligible)from a 50MHz input. 
+	clock_divider vga_pll(clk, rst, vgaclk);
+
+	// Generate monitor timing signals
+	vga_controller vgaCont(vgaclk, hsync, vsync, sync_b, blank_b, x, y);
+	// User-defined module to determine pixel color
+	video_gen videoGen(x, y, r, g, b);
+
+endmodule
+
