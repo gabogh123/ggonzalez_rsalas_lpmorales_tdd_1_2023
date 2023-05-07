c0aac3cc44eff437ef38b27089ac65a234bbdea9
LuisPeMoraRod <luismorarod98@estudiantec.cr>
2023-05-07 17:56:23 -0600

    feat(game FSM): update next state

--- game_2048/next_state.sv
+++ game_2048/next_state.sv
@@ -1,13 +1,13 @@
 module next_state(
         input logic [2:0] Q,
-        input logic buttons, won, lost, rst,
+        input logic buttons, won, gf, rst,
         output logic [2:0] D
     );
 
     always @(posedge buttons) begin
-        D[2] = Q[2] | (Q[1] & Q[0] & ~won & lost) | (Q[1] & Q[0] & won & ~lost);
-        D[1] = (Q[1] & ~Q[0]) | (Q[1] & ~won & ~lost) | (Q[1] & won & lost) | (~Q[2] & ~Q[1] & Q[0] & buttons);
-        D[0] = (Q[2] & Q[0]) | (~Q[2] & ~Q[0] & buttons) | (Q[1] & Q[0] & lost) | (~Q[1] & Q[0] & ~buttons);
+        D[2] = (Q[1] & ~won & lost) | (Q[1] & Q[0] & won & ~lost);
+        D[1] = (~Q[1] & Q[0] & buttons) | (Q[1] & ~Q[0] & ~lost) | (Q[1] & ~won & ~lost) | (Q[1] & won & lost);
+        D[0] = (~Q[0] & buttons) | (Q[1] & Q[0] & lost) | (~Q[1] & Q[0] & ~buttons) | (Q[1] & ~won & lost);
     end
 
 endmodule
--- game_2048/update_matrix.sv
+++ game_2048/update_matrix.sv
@@ -15,6 +15,11 @@ module update_matrix (
 
     // new tile where next state is 001 or 010
 	assign en_new_tile = (~Q[2] & ~Q[1] & ~Q[0]) | (~Q[2] & ~Q[1] & Q[0]) | (~Q[2] & Q[1] & Q[0]);
-	new_tile_gen new_tile(en_new_tile, buttons, rand_pos, matrix_Q, matrix_D);
+	new_tile_gen new_tile(en_new_tile, buttons, rand_pos, matrix_Q, matrix_D_0);
+	
+	// game_logic game_logic(enable, clk, rst, matrix_Q, matrix_D_1, w, l);
+
+	// assign sel = ~Q[2] & Q[1] & Q[0]; //011 
+	// mux(sel, matrix_D_0, matrix_D_1, matrix_D)
 
 endmodule
